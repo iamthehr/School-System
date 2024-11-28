1c730369631049e588efa79cf29b59639adcd0b2
iamthehr <harshraj5565@gmail.com>
2024-11-29 02:53:14 +0530

    backend deployed

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://school-system-91uu.onrender.com";
 
 export const registerUser = (userData) =>
   axios.post(`${API_URL}/auth/register`, userData);
